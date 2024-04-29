a938e100746400ef6134e49d3762b76256bd42c5
Kiril <kirilbuga@yahoo.com>
2024-04-29 20:24:07 +0200

    Update start_local.sh script
    The start_local.sh script has been modified to enable debugging with the '-x' option in the shell commands. Furthermore, a trap has been added to stop all child processes on receiving SIGINT, SIGTERM, or EXIT signals. Redundant sleep command has been removed.

--- start_local.sh
+++ start_local.sh
@@ -6,17 +6,39 @@ base_dir="$(pwd)/aoame"
 # Change directory and start fuseki server
 git_name=fuseki-heroku-test
 cd "$base_dir/$git_name"
-./fuseki-server.sh &
+sh -x fuseki-server.sh &
 
 # Change directory and start web server for web service
 git_name=OntologyBasedModellingEnvironment-WebService
 cd "$base_dir/$git_name"
-start ./start_webserver.sh &
+sh -x start_webserver.sh &
+
+# Change directory and start server for web app
+git_name=OntologyBasedModellingEnvironment-WebApp
+cd "$base_dir/$git_name"
+node server.js
+
+#!/bin/bash
+
+# Set the base directory
+base_dir="$(pwd)/aoame"
+
+# Change directory and start fuseki server
+git_name=fuseki-heroku-test
+cd "$base_dir/$git_name"
+sh -x fuseki-server.sh &
+
+# Change directory and start web server for web service
+git_name=OntologyBasedModellingEnvironment-WebService
+cd "$base_dir/$git_name"
+sh -x start_webserver.sh &
+
+# Set trap to stop all child processes when this script receives SIGINT, SIGTERM, or EXIT signals
+trap 'pkill -P $$' SIGINT SIGTERM EXIT
 
 # Change directory and start server for web app
 git_name=OntologyBasedModellingEnvironment-WebApp
 cd "$base_dir/$git_name"
 node server.js &
 
-# Optionally, add a delay to ensure the servers have started
-sleep 5
+
